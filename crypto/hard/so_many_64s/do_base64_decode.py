8a9400309be9b49ae834d2a854e809cc9f2b8992
jnasd <happy_nemo_sea@163.com>
2021-01-18 07:34:36 -0500

    CRYPTO: solve so many 64s, decode base64 recursively

---
+++ crypto/hard/so_many_64s/do_base64_decode.py
@@ -0,0 +1,14 @@
+
+import base64
+
+flag_file = open('flag.txt', 'r')
+ciphertext = flag_file.readline()
+#print(ciphertext)
+
+cipher = ciphertext
+while 1:
+    plaintext = base64.b64decode(cipher)
+    if (plaintext.find('{') != -1 and plaintext.find('CTF') != -1):
+        print(plaintext)
+        exit(0)
+    cipher = plaintext
